30f9b4af5963b6a31e4f1105075552d6e58842e5
Dominik Obermaier <dominik.obermaier@dc-quare.de>
2016-06-09 13:10:31 +0200

    fixed typo

--- src/main/java/com/hivemq/plugin/fileauthentication/FileAuthenticationModule.java
+++ src/main/java/com/hivemq/plugin/fileauthentication/FileAuthenticationModule.java
@@ -25,7 +25,7 @@ import com.hivemq.spi.plugin.meta.Information;
  *
  * @author Christian Goetz
  */
-@Information(name = "File Authentication Plugin", version = "3.0.2", author = "dc-sqare GmbH")
+@Information(name = "File Authentication Plugin", version = "3.0.2", author = "dc-square GmbH")
 public class FileAuthenticationModule extends HiveMQPluginModule {
 
 
