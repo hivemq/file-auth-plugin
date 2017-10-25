07e241ca6ec723eb340aa4f2b9f18d25738ba19c
fraschbi <florian.raschbichler@dc-square.de>
2017-10-25 16:35:42 +0200

    removed duplicate folder

--- src/main/java/com/hivemq/plugin/fileauthentication/callbacks/CredentialChangeCallback.java
+++
@@ -1,9 +0,0 @@
-package com.hivemq.plugin.fileauthentication.callbacks;
-
-/**
- * Callback to react to the change of the credentialInformation
- */
-public interface CredentialChangeCallback {
-
-    void onCredentialChange();
-}
